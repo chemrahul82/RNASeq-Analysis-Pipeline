462dc844883615f381e791a226c27af56d1ce0f3
Rahul Das <rdas@6NQ48Y1>
2016-03-30 18:31:08 -0400

    ercc_analysis module, which is under development, added

--- analysis/LS/genes_expression.py
+++ analysis/LS/genes_expression.py
@@ -7,19 +7,20 @@ import time
 
 #list of genes that Tomo has provided for LS; includes housekeeping and genes of interest
 #03/22/2016
-def ls_genes():
+def ls_genes(ref_genes):
         supportDir = '/rawdata/projects/RNA-seq/support_files'
         ls_genesList = []
-        with open('%s/ls_genes_tomo.txt' %supportDir, 'rb') as ls:
+        with open(os.path.join(supportDir,ref_genes),'rb') as ls:
+	#with open('%s/ls_genes_tomo.txt' %supportDir, 'rb') as ls:
                 for line in ls:
                         ls_genesList.append(line.strip())
         return ls_genesList
 
-def expression_genes_interest(samplename, Runid):
+def expression_genes_interest(samplename, Runid,ref_genes):
 	genes_fpkm = []
-	ls_genesList = ls_genes()
+	ls_genesList = ls_genes(ref_genes)
 	rundir=os.path.join('/rawdata/projects/RNA-seq/LS',samplename,Runid)
-	outfile = open(os.path.join(rundir,'candidate_genes_expression_%s_%s.txt' %(samplename,Runid)),'w')
+	#outfile = open(os.path.join(rundir,'candidate_genes_expression_%s_%s.txt' %(samplename,Runid)),'w')
 	
 	for file in os.listdir(rundir):	
 		if file.endswith('.genes.fpkm_tracking'):
@@ -36,11 +37,13 @@ def expression_genes_interest(samplename, Runid):
 	for lsgene in ls_genesList:
 		for idx, genes in enumerate(genename):
 			if lsgene == genes:
-				#print('\t'.join([lsgene, fpkm[idx]]))	
-				outfile.write('\t'.join([lsgene, fpkm[idx]])+'\n')
-	outfile.close()
+				print('\t'.join([lsgene, fpkm[idx]]))	
+				#outfile.write('\t'.join([lsgene, fpkm[idx]])+'\n')
+	#outfile.close()
 if __name__ == "__main__":
 	samplename = sys.argv[1]
 	runid = sys.argv[2]
-	expression_genes_interest(samplename, runid)
+	ref_genes = sys.argv[3]
+	expression_genes_interest(samplename, runid, ref_genes)
+
 			
